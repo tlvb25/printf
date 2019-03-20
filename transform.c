79aee82fd7104ae2883258192146f4d6027951ef
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-03-19 22:44:35 -0700

    made change to function

--- transform.c
+++ transform.c
@@ -26,3 +26,30 @@ char *convert(unsigned int num, int base)
 
 	return (ptr);
 }
+
+
+/**
+ * print_conv - function converts input to specified base
+ * @num: the input number
+ * @base: the specified base
+ * @cap: a flag to print hex in capital. 0 is lowercase
+ * Return: number of characters printed
+ */
+char *print_conv(unsigned long int num, int base, int cap)
+{
+	char *string = "0123456789abcdef";
+	char *String = "0123456789ABCDEF";
+	char arr[50];
+	char *ptr;
+
+	ptr = &arr[49];
+	*ptr = '\0';
+
+	if (cap)
+		string = String;
+	do	{
+		*--ptr = string[num % base];
+		num /= base;
+	} while (num);
+	return (ptr);
+}
